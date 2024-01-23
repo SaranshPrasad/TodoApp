6e90bd5c4e628649cd1b334cf5b22cb8c2820b86
SaranshPrasad <saranshprasad08@gmail.com>
2024-01-23 20:10:35 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const EventEmitter = require("events");
+const event = new EventEmitter();
+const app = express();
+const port = 3000;
+
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+let todoTasks= [];
+
+
+
+app.get('/', (req , res ) => {
+    res.render("index.ejs" , {todoTasks});
+});
+
+
+app.post("/" , (req , res ) =>{
+      const newTask = req.body.task;
+      const newItem = { id: todoTasks.length , text: newTask };
+      todoTasks.push(newItem);
+    res.redirect('/');
+});
+
+app.post('/:id', (req, res) => {
+    const itemId = parseInt(req.params.id);
+    todoTasks = todoTasks.filter(item => item.id !== itemId);
+    res.redirect('/');
+  });
+
+
+
+
+app.listen(port , () => {
+    console.log(`Server is live on ${port} !`);
+});
---
+++ public/styles/client.js
@@ -0,0 +1,5 @@
+let btn = document.getElementsByTagName["button"];
+
+btn.addEventListener("click" , ( ) => {
+    alert("Button Clicked !");
+})
