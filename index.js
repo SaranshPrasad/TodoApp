214502d8d79ab67a201aeedc80dee55f8245e3f2
SaranshPrasad <131943503+SaranshPrasad@users.noreply.github.com>
2024-01-23 20:32:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const EventEmitter = require("events");
 const event = new EventEmitter();
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3030
 
 
 app.use(express.static("public"));
